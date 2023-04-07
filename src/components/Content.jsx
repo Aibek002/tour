36e2f94fc9aa97f63c7edc739b01428adc654d1d
Aibek002 <200103346@stu.sdu.edu.kz>
2023-04-07 11:47:30 +0600

    fours commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,15 +3,54 @@ import './content.css'
 const Content = () => {
     return (
         <div className="content">
-         <div className="ReclamImg"><img src="" width="100%" alt=""  />  </div>
-         <div className="a"></div>
-         <div className="b"></div>
-         <div className="c"></div>
-         <div className="d"></div>
-         <div className="e"></div>
-         <div className="f"></div>
-         <div className="g"></div>
-       
+            <div className="Reclam"> <p className="first">Find your special tour today</p> <p className="second">With S-Kazakh tour</p> </div>
+        <nav className="a">
+                <div className="firstDiv">  <div className="f">     <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/7509/7509196.png" alt="" /></div>
+                    <div className="bs">
+                        <p className="titleText">Diverse Destinations</p>
+                        <p className="text">Habitasse. Nunc mollis erat dapibus in commodo eu nisi. Odio purus magnis lorem.</p>
+                    </div></div>
+
+                    <div className="b">     <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/3083/3083713.png" alt="" /></div>
+                        <div className="bs">
+                            <p className="titleText">Diverse Destinations</p>
+                            <p className="text">Habitasse. Nunc mollis erat dapibus in commodo eu nisi. Odio purus magnis lorem.</p>
+                        </div></div>
+
+                    <div className="c"> <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/3083/3083713.png" alt="" /></div>
+                        <div className="bs">
+                            <p className="titleText">Diverse Destinations</p>
+                            <p className="text">Habitasse. Nunc mollis erat dapibus in commodo eu nisi. Odio purus magnis lorem.</p>
+                        </div></div></div>
+
+
+                <div className="secondDiv"> <div className="f">     <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/7509/7509196.png" alt="" /></div>
+                    <div className="bs">
+                        <p className="titleText">Diverse Destinations</p>
+                        <p className="text">Habitasse. Nunc mollis erat dapibus in commodo eu nisi. Odio purus magnis lorem.</p>
+                    </div></div>
+
+                    <div className="b">     <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/3083/3083713.png" alt="" /></div>
+                        <div className="bs">
+                            <p className="titleText">Diverse Destinations</p>
+                            <p className="text">Habitasse. Nunc mollis erat dapibus in commodo eu nisi. Odio purus magnis lorem.</p>
+                        </div></div>
+
+                    <div className="c">
+                        <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/3083/3083713.png" alt="" /></div>
+                        <div className="bs">
+                            <p className="titleText">Diverse Destinations</p>
+                            <p className="text">Habitasse. Nunc mollis erat dapibus in commodo eu nisi. Odio purus magnis lorem.</p>
+                        </div></div></div>
+        </nav>
+
+            <div className="b"></div>
+            <div className="c"></div>
+            <div className="d"></div>
+            <div className="e"></div>
+            <div className="f"></div>
+            <div className="g"></div>
+
         </div>
     );
 }
