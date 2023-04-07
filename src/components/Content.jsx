d7000ce148fe2f0898476b8762105b7b9c79d336
Aibek002 <200103346@stu.sdu.edu.kz>
2023-04-07 10:41:34 +0600

    third commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,7 @@ import './content.css'
 const Content = () => {
     return (
         <div className="content">
-         <div className="ReclamImg"><img src="https://images.wallpaperscraft.ru/image/single/puteshestvenniki_puteshestvie_turizm_169200_300x168.jpg" alt=""  /></div>
+         <div className="ReclamImg"><img src="" width="100%" alt=""  />  </div>
          <div className="a"></div>
          <div className="b"></div>
          <div className="c"></div>
