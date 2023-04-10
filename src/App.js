5d47672d8759562fb9ff19236c69849184c3c23c
Aibek002 <200103346@stu.sdu.edu.kz>
2023-04-10 18:53:19 +0600

    move to file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Content from './components/Content';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Content from './components/content/Content';
 
 function App() {
   return (
--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -5,26 +5,26 @@ const Content = () => {
         <div className="content">
             <div className="Reclam"> <p className="first">Find your special tour today</p> <p className="second">With S-Kazakh tour</p> </div>
         <nav className="a">
-                <div className="firstDiv">  <div className="f">     <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/7509/7509196.png" alt="" /></div>
+                <div className="firstDiv">  <div className="f">     <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/512/10238/10238071.png" alt="" /></div>
                     <div className="bs">
                         <p className="titleText">Diverse Destinations</p>
                         <p className="text">Habitasse. Nunc mollis erat dapibus in commodo eu nisi. Odio purus magnis lorem.</p>
                     </div></div>
 
-                    <div className="b">     <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/3083/3083713.png" alt="" /></div>
+                    <div className="b">     <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/512/8690/8690414.png" alt="" /></div>
                         <div className="bs">
                             <p className="titleText">Diverse Destinations</p>
                             <p className="text">Habitasse. Nunc mollis erat dapibus in commodo eu nisi. Odio purus magnis lorem.</p>
                         </div></div>
 
-                    <div className="c"> <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/3083/3083713.png" alt="" /></div>
+                    <div className="c"> <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/10088/10088309.png" alt="" /></div>
                         <div className="bs">
                             <p className="titleText">Diverse Destinations</p>
                             <p className="text">Habitasse. Nunc mollis erat dapibus in commodo eu nisi. Odio purus magnis lorem.</p>
                         </div></div></div>
 
 
-                <div className="secondDiv"> <div className="f">     <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/7509/7509196.png" alt="" /></div>
+                <div className="secondDiv"> <div className="f">     <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/512/10021/10021238.png" alt="" /></div>
                     <div className="bs">
                         <p className="titleText">Diverse Destinations</p>
                         <p className="text">Habitasse. Nunc mollis erat dapibus in commodo eu nisi. Odio purus magnis lorem.</p>
@@ -37,7 +37,7 @@ const Content = () => {
                         </div></div>
 
                     <div className="c">
-                        <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/128/3083/3083713.png" alt="" /></div>
+                        <div className="as"><img className="imgLogo" src="https://cdn-icons-png.flaticon.com/512/7536/7536643.png" alt="" /></div>
                         <div className="bs">
                             <p className="titleText">Diverse Destinations</p>
                             <p className="text">Habitasse. Nunc mollis erat dapibus in commodo eu nisi. Odio purus magnis lorem.</p>
