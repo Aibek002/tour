e8c57f9422bd76a89deb04dd52e1e088c2e4bf1f
Aibek002 <200103346@stu.sdu.edu.kz>
2023-04-11 12:58:32 +0600

    move to file commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import './header.css'
+import s from './header.module.css'
 
 const Header = () => {
     return (
-        <header className="header">
-            <div className="logo"><img src="https://cdn.cdnlogo.com/logos/a/42/as-design.svg" alt="" width="80px" height="60px" /></div>
-            <div className="empty"></div>
-            <div className="empty"></div>
-            <div className="empty"></div>
-            <div className="site1">Home</div>
-            <div className="site2">Tour</div>
-            <div className="site3">Price</div>
-            <div className="site4">Blog</div>
+        <header className={s.header}>
+            <div className={s.logo}><img src="https://cdn.cdnlogo.com/logos/a/42/as-design.svg" alt="" width="80px" height="60px" /></div>
+            <div className={s.empty}></div>
+            <div className={s.empty}></div>
+            <div className={s.empty}></div>
+            <div className={s.site1}>Home</div>
+            <div className={s.site2}>Tour</div>
+            <div className={s.site3}>Price</div>
+            <div className={s.site4}>Blog</div>
 
 
         </header>
